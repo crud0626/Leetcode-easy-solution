cc0ee6f5261dd0586923bbcd73e7c3c629891cd1
crud0626 <sisofiy6048@gmail.com>
2022-05-02 11:47:19 +0900

    Add problem 35

---
+++ easy/35-Search-Insert-Position.js
@@ -0,0 +1,15 @@
+// Runtime: 58ms
+// Memory Usage: 42.2MB
+
+const searchInsert = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] === target || nums[i] > target) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
