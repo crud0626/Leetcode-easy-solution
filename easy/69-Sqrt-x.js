c9edf7d9c6e4a4843a0a5182b4fed80b79940a95
crud0626 <sisofiy6048@gmail.com>
2022-05-05 13:34:44 +0900

    Add problem 69

---
+++ easy/69-Sqrt-x.js
@@ -0,0 +1,13 @@
+// 68ms, 96.76%
+// 43MB
+
+const mySqrt = function(x) {
+    return parseInt(Math.sqrt(x));
+};
+
+console.log(mySqrt(4)); // 2
+
+console.log(mySqrt(8)); // 2
+
+// = Math.pow(x, 0.5)
+// = x ** 0.5
