e0994723345470565f3076557ae558bdd366993b
crud0626 <sisofiy6048@gmail.com>
2022-05-23 13:57:52 +0900

    Add problem 344

---
+++ easy/344-Reverse-String.js
@@ -0,0 +1,10 @@
+// 91ms, 90.09%
+// 49.2mb
+
+const reverseString = function(s) {
+    return s.reverse();
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
+
+console.log(reverseString(["H","a","n","n","a","h"]));
