7e5834560371fbdbc3fcc950e464b1f2e99c46b2
crud0626 <sisofiy6048@gmail.com>
2022-06-27 12:17:40 +0900

    Add problem 709

---
+++ easy/709-ToLowerCase.js
@@ -0,0 +1,12 @@
+// 60ms, 93.92%
+// 42.4MB
+
+const toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+
+console.log(toLowerCase("Hello"));
+
+console.log(toLowerCase("here"));
+
+console.log(toLowerCase("LOVELY"));
