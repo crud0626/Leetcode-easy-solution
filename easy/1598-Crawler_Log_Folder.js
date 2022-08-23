89d5bc96e65023d4f104eb726247fb2ac1d8ff43
crud0626 <sisofiy6048@gmail.com>
2022-08-23 10:44:09 +0900

    Add problem 1598

---
+++ easy/1598-Crawler_Log_Folder.js
@@ -0,0 +1,29 @@
+// 
+// 
+
+const minOperations = function(logs) {
+    let minNums = 0;
+
+    for(let i = 0; i < logs.length; i++) {
+        const target = logs[i];
+
+        switch(true) {
+            case target.match(/\w+\//) !== null:
+                minNums += 1;
+                break;
+            case target.match(/\.{2}\//) !== null && minNums > 0:
+                minNums -= 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return minNums;
+};
+
+console.log(minOperations(["d1/","d2/","../","d21/","./"]));
+
+console.log(minOperations(["d1/","d2/","./","d3/","../","d31/"]));
+
+console.log(minOperations(["d1/","../","../","../"]));
